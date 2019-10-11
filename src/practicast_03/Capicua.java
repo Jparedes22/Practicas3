dd65f5ba141849c45c7f99f4c821c666681b0160
Usuario DAM 1 <Usuario DAM 1@DESKTOP-R6MG806>
2019-10-11 10:28:50 +0200

    PRACTICAS5

--- src/practicast_03/Capicua.java
+++ src/practicast_03/Capicua.java
@@ -12,24 +12,24 @@ import java.util.Scanner;
  * @author Usuario DAM 1
  */
 public class Capicua {
+
     public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    int num;
+        Scanner sc = new Scanner(System.in);
+        int num;
         System.out.println("Introduce un número de 4 dígitos");
-        num=sc.nextInt();
-        int num1=num/1000;
-        int num2=(num-(num1*1000))/100;
-        int num3=(num-(num1*1000+num2*100))/10;
-        int num4=num-(num1*1000+num2*100+num3*10);
-        
-        
-        if(num<999||num>10000){
+        num = sc.nextInt();
+        int num1 = num / 1000;
+        int num2 = (num - (num1 * 1000)) / 100;
+        int num3 = (num - (num1 * 1000 + num2 * 100)) / 10;
+        int num4 = num - (num1 * 1000 + num2 * 100 + num3 * 10);
+
+        if (num < 999 || num > 10000) {
             System.out.println("El número no tiene 4 dígitos, listillo");
-        }else if(num1==num4&&num2==num3){
+        } else if (num1 == num4 && num2 == num3) {
             System.out.println("El número es capicúa");
-    }else{
+        } else {
             System.out.println("El número no es capicúa ");
         }
-        
+
     }
 }
